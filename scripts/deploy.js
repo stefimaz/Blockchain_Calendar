85d8fd36128ee2a53595b16a4d575b3823304465
stefimaz <89653430+stefimaz@users.noreply.github.com>
2022-03-11 12:22:01 -0500

    Deploy.js updated

---
+++ scripts/deploy.js
@@ -0,0 +1,51 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Contract = await hre.ethers.getContractFactory("CalendarEth");
+  const contract = await Contract.deploy();
+
+  await contract.deployed();
+
+  saveFrontendFiles();
+
+  console.log("CalendarEth deployed to:", contract.address);
+}
+
+function saveFrontendFiles() {
+  const fs = require("fs");
+
+  const abiDir = __dirname + "/../frontend/src/abis";
+
+  if (!fs.existsSync(abiDir)) {
+    fs.mkdirSync(abiDir);
+  }
+
+  const artifact = artifacts.readArtifactSync("CalendarEth");
+
+  fs.writeFileSync(
+    abiDir + "/CalendarEth.json",
+    JSON.stringify(artifact, null, 2)
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
